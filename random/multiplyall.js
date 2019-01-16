5e59e3dc7f051f80118887d508b64c9fc2f99b6e
obayomi96 <martinsoluwaseun47@gmail.com>
2019-01-16 12:55:23 +0000

    multiply elements in a nested array

--- random/multiplyall.js
+++ random/multiplyall.js
@@ -3,8 +3,8 @@
 function multiplyAll(arr) {
     var product = 1;
     
-    for (var i =0; i< arr.length; i) {
-        for (var j=0; j< arr[i].length; j++) {
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
             product *= arr[i][j];
         }
     }
